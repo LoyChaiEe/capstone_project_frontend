e11a20b7e6d376ef8d393ea893d4014664b55721
Nicklye26 <nicklye26@gmail.com>
2023-03-22 19:19:13 +0800

    mock profile page is done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import "./profile.css";
 
 export default function Profile() {
   return (
     <div className="profile-section">
       <div className="profile-container">
-        <span>PROFILE</span>
+        <div className="profile-image-wrapper">
+          <img
+            src={require("../assets/profile.png")}
+            className="profile-image"
+            alt="profile-pic"
+          />
+        </div>
+        <div className="profile-info-wrapper">
+          <h1 className="profile-info">Name:</h1>
+          <input className="profile-input-box" value={"nicholas"} />
+        </div>
+        <div className="profile-info-wrapper">
+          <h1 className="profile-info">Username:</h1>
+          <input className="profile-input-box" value={"nicholas"} />
+        </div>
+        <div className="profile-info-wrapper">
+          <h1 className="profile-info">Email Address:</h1>
+          <input className="profile-input-box" value={"nicholas@gmail.com"} />
+        </div>
+        <div className="profile-info-wrapper">
+          <h1 className="profile-info">Country:</h1>
+          <input className="profile-input-box" value={"singapore"} />
+        </div>
+        <button>Edit Profile</button>
       </div>
     </div>
   );
