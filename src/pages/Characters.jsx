5738265b33c644dd3bad9c4b0fc7aad4b8e81720
Nicklye26 <nicklye26@gmail.com>
2023-04-01 06:51:47 +0800

    rendered all japanese characters in character page

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,15 +1,34 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Backend_URL } from "../BACKEND_URL";
+import "./characters.css";
 
 export default function Characters() {
-  const [words, setWords] = useState({});
+  // const [words, setWords] = useState({});
+  const [hiraganaBasic, setHiraganaBasic] = useState([]);
+  const [hiraganaDakuon, setHiraganaDakuon] = useState([]);
+  const [katakanaBasic, setKatakanaBasic] = useState([]);
+  const [katakanaDakuon, setKatakanaDakuon] = useState([]);
 
   useEffect(() => {
     const getWords = async () => {
       try {
-        const characters = await axios.get(`${Backend_URL}/words`);
-        setWords(characters);
+        const hiragana_basic = await axios.get(
+          `${Backend_URL}/words/hiraganaBasic`
+        );
+        setHiraganaBasic(hiragana_basic);
+        const hiragana_dakuon = await axios.get(
+          `${Backend_URL}/words/hiraganaDakuon`
+        );
+        setHiraganaDakuon(hiragana_dakuon);
+        const katakana_basic = await axios.get(
+          `${Backend_URL}/words/katakanaBasic`
+        );
+        setKatakanaBasic(katakana_basic);
+        const katakana_dakuon = await axios.get(
+          `${Backend_URL}/words/katakanaDakuon`
+        );
+        setKatakanaDakuon(katakana_dakuon);
       } catch (error) {
         console.log("error", error);
       }
@@ -17,19 +36,73 @@ export default function Characters() {
     getWords();
   }, []);
 
-  useEffect(() => {
-    const wordsData = words.data;
-    if (wordsData !== undefined) {
-      wordsData.map((i) => {
-        return console.log("wordsData", i.character);
-      });
-    }
-  });
+  const hiraganaBasicsData = hiraganaBasic.data;
+  const hiraganaDakuonData = hiraganaDakuon.data;
+  const katakanaBasicsData = katakanaBasic.data;
+  const katakanaDakuonData = katakanaDakuon.data;
+
+  // useEffect(() => {
+  //   if (hiraganaBasicsData !== undefined && hiraganaDakuonData !== undefined) {
+  //     hiraganaBasicsData.map((i) => {
+  //       return console.log("hiragana basic", i);
+  //     });
+  //     hiraganaDakuonData.map((i) => {
+  //       return console.log("hiragana dakuon", i);
+  //     });
+  //   }
+  // });
 
   return (
     <div className="character-section">
       <div className="character-container">
-        <span>CHARACTERS</span>
+        <h1>Hiragana Basic</h1>
+        <div className="hirgana-basic-grid">
+          {hiraganaBasicsData &&
+            hiraganaBasicsData.map((i) => (
+              <div className="character-wrapper" key={i.id}>
+                <div className="character">{i.character}</div>
+                <div className="character-lower-wrapper">
+                  <div className="pronounciation">{i.pronounciation}</div>
+                </div>
+              </div>
+            ))}
+        </div>
+        <h1>Hiragana Dakuon</h1>
+        <div className="hirgana-basic-grid">
+          {hiraganaDakuonData &&
+            hiraganaDakuonData.map((i) => (
+              <div className="character-wrapper" key={i.id}>
+                <div className="character">{i.character}</div>
+                <div className="character-lower-wrapper">
+                  <div className="pronounciation">{i.pronounciation}</div>
+                </div>
+              </div>
+            ))}
+        </div>
+        <h1>Katakana Basic</h1>
+        <div className="hirgana-basic-grid">
+          {katakanaBasicsData &&
+            katakanaBasicsData.map((i) => (
+              <div className="character-wrapper" key={i.id}>
+                <div className="character">{i.character}</div>
+                <div className="character-lower-wrapper">
+                  <div className="pronounciation">{i.pronounciation}</div>
+                </div>
+              </div>
+            ))}
+        </div>
+        <h1>Katakana Dakuon</h1>
+        <div className="hirgana-basic-grid">
+          {katakanaDakuonData &&
+            katakanaDakuonData.map((i) => (
+              <div className="character-wrapper" key={i.id}>
+                <div className="character">{i.character}</div>
+                <div className="character-lower-wrapper">
+                  <div className="pronounciation">{i.pronounciation}</div>
+                </div>
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
