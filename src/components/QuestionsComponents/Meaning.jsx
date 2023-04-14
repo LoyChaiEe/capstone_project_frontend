e07e19df3e4ac820e336759eb1a431fc443082a6
Nicklye26 <nicklye26@gmail.com>
2023-04-14 22:46:20 +0800

    user able to select word and click on submit, need to work on answer verification

--- src/components/QuestionsComponents/Meaning.jsx
+++ src/components/QuestionsComponents/Meaning.jsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from "react";
 import { MiniCharacter } from "../SVG";
 import { Backend_URL } from "../../BACKEND_URL";
 import axios from "axios";
-import Button from "../Button";
+// import Button from "../Button";
+import { Button } from "antd";
 import "./meaning.css";
 
 export default function Meaning(props) {
   const questionData = props.questionData;
   const wordBank = props.wordBank;
   const [wordArray, setWordArray] = useState([]);
+  const [wordSelected, setWordSelected] = useState("");
   const type = questionData?.question_type.split("-");
 
   // to display 4 words in an array
@@ -25,15 +27,28 @@ export default function Meaning(props) {
     // resets array after every question
     setWordArray([]);
   }, []);
-  console.log(wordArray);
+
+  // user selects word
+  const select = (e) => {
+    e.preventDefault();
+    const selected = e.target.textContent;
+    setWordSelected(selected);
+  };
 
   const wordArrayDisplay = wordArray.map((wordArray) => (
-    <button>{wordArray.character}</button>
+    <Button onClick={select}>{wordArray.character}</Button>
   ));
+
+  if (wordArray.length !== 0) {
+    props.canSubmit(true);
+  } else {
+    props.canSubmit(false);
+  }
 
   return (
     <>
       <span>{questionData.question}</span>
+      <div>{wordSelected}</div>
       <div>{wordArrayDisplay}</div>
     </>
   );
