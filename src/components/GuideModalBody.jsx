3d757b26dcbb8c2b9426ee2e8ce391db6e203ab8
Nicklye26 <nicklye26@gmail.com>
2023-04-20 20:35:48 +0800

    added soundplay function when user click on word in guide modal

--- src/components/GuideModalBody.jsx
+++ src/components/GuideModalBody.jsx
@@ -2,9 +2,15 @@ import React, { useEffect, useState } from "react";
 import "./guideModalBody.css";
 import axios from "axios";
 import { Backend_URL } from "../BACKEND_URL";
+import { useOutletContext } from "react-router-dom";
+import { Howl } from "howler";
 
 export default function GuideModalBody() {
   const [lessonWords, setLessonWords] = useState([]);
+  const [userData] = useOutletContext();
+
+  const speaker = userData.voicevox_id;
+
   useEffect(() => {
     try {
       axios.get(`${Backend_URL}/LQA/lesson/words/${1}`).then((res) => {
@@ -17,11 +23,54 @@ export default function GuideModalBody() {
 
   console.log(lessonWords);
 
+  const play = async (e) => {
+    const text = e.target.textContent;
+    console.log(text);
+    const data = await createAudio(text);
+    const audioSRC = URL.createObjectURL(data);
+    console.log(audioSRC);
+    const sound = new Howl({
+      src: [audioSRC],
+      autoplay: false,
+      loop: false,
+      volume: 1,
+      format: "wav",
+    });
+    sound.play();
+  };
+
+  const createQuery = async (text) => {
+    //change speaker query to the id of the waifu
+    const response = await axios.post(
+      `http://localhost:50021/audio_query?speaker=${speaker}&text=${text}`
+    );
+    return response.data;
+  };
+
+  const createVoice = async (text) => {
+    const query = await createQuery(text);
+    const response = await axios.post(
+      `http://localhost:50021/synthesis?speaker=${speaker}`,
+      query,
+      { responseType: "blob" }
+    );
+    return response.data;
+  };
+
+  const createAudio = async (text) => {
+    const data = await createVoice(text);
+    return data;
+  };
+
   return (
     <div>
       <h1>Words in Lesson 1!</h1>
       {lessonWords.map((words, index) => {
-        return <div key={index}>{words.character.character}</div>;
+        return (
+          <button onClick={play} key={index}>
+            {words.character.character}
+          </button>
+        );
       })}
     </div>
   );
